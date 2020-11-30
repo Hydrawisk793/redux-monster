828e296f0de83bb4dbc49b0d9a03b30508896a1c
Hydrawisk793 <hyw793@gmail.com>
2020-11-30 13:38:13 +0000

    feat!: ooops

--- src/ts-utils.d.ts
+++ src/ts-utils.d.ts
@@ -1,7 +1,9 @@
-export declare type PickExtends<S, T> = {
-    [ K2 in { [ K in keyof S ] : (S[K] extends T ? K : never) }[keyof S] ] : S[K2];
-};
+export declare type PickExtends<S, T> = Pick<
+    S,
+    { [ K in keyof S ] : (S[K] extends T ? K : never) }[keyof S]
+>;
 
-export declare type PickReturnTypes<S> = {
-    [ K2 in { [ K in keyof S ] : (S[K] extends (...args : any[]) => any ? ReturnType<S[K]> : never) }[keyof S] ] : S[K2];
-};
+export declare type PickReturnTypes<S> = Pick<
+    S,
+    { [ K in keyof S ] : (S[K] extends (...args : any[]) => any ? K : never) }[keyof S]
+>;
